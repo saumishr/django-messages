774c0b343355c56d36d6c6ee40a9ee422334c5df
saumishr <saumishr@ubuntu.(none)>
2013-04-27 11:28:09 +0530

    Initial version of django-messages with django-notification create_notice_type bug fix and base.html changes

--- django_messages/management.py
+++ django_messages/management.py
@@ -5,14 +5,14 @@ from django.utils.translation import ugettext_noop as _
 if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
 
-    def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("messages_received", _("Message Received"), _("you have received a message"), default=2)
-        notification.create_notice_type("messages_sent", _("Message Sent"), _("you have sent a message"), default=1)
-        notification.create_notice_type("messages_replied", _("Message Replied"), _("you have replied to a message"), default=1)
-        notification.create_notice_type("messages_reply_received", _("Reply Received"), _("you have received a reply to a message"), default=2)
-        notification.create_notice_type("messages_deleted", _("Message Deleted"), _("you have deleted a message"), default=1)
-        notification.create_notice_type("messages_recovered", _("Message Recovered"), _("you have undeleted a message"), default=1)
+    def create_notifications(app, created_models, verbosity, **kwargs):
+        notification.NoticeType.create("messages_received", _("Message Received"), _("you have received a message"), default=2)
+        notification.NoticeType.create("messages_sent", _("Message Sent"), _("you have sent a message"), default=1)
+        notification.NoticeType.create("messages_replied", _("Message Replied"), _("you have replied to a message"), default=1)
+        notification.NoticeType.create("messages_reply_received", _("Reply Received"), _("you have received a reply to a message"), default=2)
+        notification.NoticeType.create("messages_deleted", _("Message Deleted"), _("you have deleted a message"), default=1)
+        notification.NoticeType.create("messages_recovered", _("Message Recovered"), _("you have undeleted a message"), default=1)
 
-    signals.post_syncdb.connect(create_notice_types, sender=notification)
+    signals.post_syncdb.connect(create_notifications, sender=notification)
 else:
     print "Skipping creation of NoticeTypes as notification app not found"
